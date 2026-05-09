fba27d17f77f0f8b20cff9cb7ad7e070a146b248
Girish Ranade <granade@seek.com.au>
2026-05-09 23:41:47 +1000

    Added some sample data in comments

--- src/calculateOrders.ts
+++ src/calculateOrders.ts
@@ -32,13 +32,44 @@ export const calculateOrders = (): Result[] => {
   );
 
   // INFO: Creating data structure to aggregate sales data by productId and salesDate
+  /** 
+   * @aggregateData :
+   * {'P1-21/04/2026': {
+        orderIds: [ 'O10', 'O20', 'O31' ],
+        customerIds: [ 'C1', 'C2', 'C3' ],
+        salesDate: '21/04/2026',
+        productId: 'P1',
+        productName: 'Ezy Storage 37L Flexi Laundry Basket - White',
+        totalSales: 4,
+        productSales: 3
+  },}
+   */
   const aggregateData = getAggregateData(ordersData, productMap);
 
 
 // INFO: Finding the top-selling product for each day
+/**
+ * @maxSoldProductsPerDay :
+ * '21/04/2026': {
+        salesDate: '21/04/2026',
+        productId: 'P1',
+        productName: 'Ezy Storage 37L Flexi Laundry Basket - White',
+        productSales: 3,
+        orderIds: [ 'O10', 'O20', 'O31' ],
+        customerIds: [ 'C1', 'C2', 'C3' ],
+        totalSales: 4
+  },
+ */
   const maxSoldProductsPerDay = getMaxSoldProductPerDay(aggregateData);
 
   // INFO: Creating a data structure to store the top-selling product for each day
+  /**
+   * @productPerDay :
+    * [{
+        date: '21/04/2026',
+        productName: 'Ezy Storage 37L Flexi Laundry Basket - White'
+    }]
+   */
   const productPerDay = Object.entries(maxSoldProductsPerDay).map(
     ([date, product]) => ({
       date,
@@ -46,7 +77,10 @@ export const calculateOrders = (): Result[] => {
     }),
   );
 
+  
+
   // INFO: Generating results in the required format with date range and product name
+  
   const results = getDateRangeProductResult(productPerDay);
   return results;
 };
